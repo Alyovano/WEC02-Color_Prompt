cde5787c87d3623c4ff63f9c62445693d32a9408
Alyovano <56916755+Alyovano@users.noreply.github.com>
2020-04-26 21:42:46 +0200

    Version finale

--- color.c
+++ color.c
@@ -1,5 +1,14 @@
 #include "color.h"
 
+int parse_color(char **av, color *rgb)
+{
+    rgb->r = ft_atoi(av[1]);
+    rgb->g = ft_atoi(av[2]);
+    rgb->b = ft_atoi(av[3]);
+    valid_rgb(rgb);
+    return (-1);
+}
+
 int prompt(color *rgb)
 {
     char *input;
@@ -10,7 +19,7 @@ int prompt(color *rgb)
         ft_printf("\033[38;0;255;255;255m%s", "_WEC02_ > ");
         if (get_next_line(0, &input) == -1)
             return (my_strerror(1, 0, "Malloc failed\n"));
-        if (exit_pls(input, "exit") == 1)
+        if (exit_pls(input, "!exit") == 1)
             break ;
         ft_printf("\033[38;2;%d;%d;%dm%s", rgb->r, rgb->g, rgb->b, input);
         if (*input != '\0')
@@ -18,15 +27,6 @@ int prompt(color *rgb)
         free(input);
     }
     return (1);
-}
-
-int parse_color(char **av, color *rgb)
-{
-    rgb->r = ft_atoi(av[1]);
-    rgb->g = ft_atoi(av[2]);
-    rgb->b = ft_atoi(av[3]);
-    valid_rgb(rgb);
-    return (-1);
 }
 
 int main(int ac, char **av)
@@ -39,7 +39,7 @@ int main(int ac, char **av)
     {
         parse_color(av, rgb);
         ft_printf("R%d G%d B%d\n", rgb->r, rgb->g, rgb->b);
-        ft_printf("If you want to leave just type: $>exit\n");
+        ft_printf("If you want to leave just type: $>!exit\n");
         if (prompt(rgb) == 1)
             ft_printf("Goodbye !\n");
         free(rgb);
